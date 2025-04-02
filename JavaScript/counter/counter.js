9add9ffaffb2b00f2c6d814d39ae0dd98e442825
Woody Lin <wancheng.lin001@umb.edu>
2025-04-01 23:57:21 -0400

    Update counter.js

--- JavaScript/counter/counter.js
+++ JavaScript/counter/counter.js
@@ -1,9 +1,7 @@
-
-
 let count = 0;
+
 const number = document.getElementById('count');
 const button = document.querySelector('#btn')
-
 
 // add click event listener
 button.addEventListener('click', ()=>{
@@ -12,12 +10,85 @@ button.addEventListener('click', ()=>{
 })
 
 
+
+// using closure
+function createCounter() {
+    let count = 0;
+
+    return {
+        getCount: function(){
+            return count;
+        },
+        increment: function(){
+            count++;
+            return count;
+        },
+        reset: function(){
+            count = 0;
+            return count;
+        }
+    }
+}
+const number2 = document.getElementById('count2');
+const button2 = document.querySelector('#btn2');
+
+// create an object
+const counter = createCounter();
+button2.addEventListener('click', ()=>{
+    counter.increment();
+    number2.textContent = counter.getCount();
+})
+
+
+
+// using class
+class Counter{
+    #count = 0; // private field (ES2022)
+
+    constructor(){}
+
+    getCount(){
+        return this.#count;
+    }
+
+    increment(){
+        this.#count++;
+        return this.#count;
+    }
+
+    reset(){
+        this.#count = 0;
+        return this.#count;
+    }
+}
+const number3 = document.getElementById('count3');
+const button3 = document.querySelector('#btn3');
+
+// create an object
+const counter2 = new Counter();
+button3.addEventListener('click', ()=>{
+    counter2.increment();
+    number3.textContent = counter2.getCount();
+})
+
+
+
+
+
 // reset
 const reset = document.querySelector('.reset');
 
 reset.addEventListener('click', ()=>{
     count = 0;
     number.textContent = count;
+
+    // reset closure counter
+    counter.reset();
+    number2.textContent = counter.getCount();
+
+    // reset class counter
+    counter2.reset();
+    number3.textContent = counter2.getCount();
 })
 
 
