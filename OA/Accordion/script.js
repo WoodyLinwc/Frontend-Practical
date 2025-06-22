d5f1125af0b36704e0f52ac89c8e7095735be977
Woody Lin <wancheng.lin001@umb.edu>
2025-06-22 00:46:43 -0400

    accordion

--- OA/Accordion/script.js
+++ OA/Accordion/script.js
@@ -15,6 +15,43 @@ const getMealType = (data, type) => {
         .map((recipe) => recipe.name);
 };
 
+const toggleAccordion = (header) => {
+    const content = header.nextElementSibling;
+
+    const currentDisplay = window.getComputedStyle(content).display;
+
+    if (currentDisplay === "none") {
+        content.style.display = "block"; // Show
+    } else {
+        content.style.display = "none"; // Hide
+    }
+};
+
+const createAccordion = (mealsByType) => {
+    const accordion = document.getElementById("accordion");
+    accordion.innerHTML = "";
+
+    Object.entries(mealsByType).forEach(([mealType, recipes]) => {
+        const accordionItem = document.createElement("div");
+        accordionItem.className = "accordion-item";
+
+        accordionItem.innerHTML = `
+
+            <div onclick="toggleAccordion(this)">
+                <span>${mealType}</span>
+            </div>
+            <div>
+                <ul>
+                    ${recipes.map((recipe) => `<li>${recipe}</li>`).join("")}
+                </ul>
+            </div>
+        
+        `;
+
+        accordion.appendChild(accordionItem);
+    });
+};
+
 const init = async () => {
     try {
         const data = await fetchData(url);
@@ -30,10 +67,12 @@ const init = async () => {
         arrayTypes.forEach((type) => {
             mealsByType[type] = getMealType(data, type);
         });
+        //  {Appetizer: [ 'Tomato Basil Bruschetta' ],Snacks: [ 'Chapli Kebabs' ]...}
 
         // get all name that mealType manually
         // const lunch = getMealType(data, "Lunch")
-        console.log(mealsByType);
+
+        createAccordion(mealsByType);
     } catch (error) {
         console.error("Error fetching data", error);
     }
