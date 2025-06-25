27a6450ed8bdd128da2969e0bc523233419bafba
Woody Lin <wancheng.lin001@umb.edu>
2025-06-24 22:15:51 -0400

    Update script.js

--- OA/JS/Search Bar/script.js
+++ OA/JS/Search Bar/script.js
@@ -1,19 +1,50 @@
 const url = "https://dummyjson.com/recipes";
 
+const allNames = [];
+
 const fetchData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
-
     return data;
+};
+
+const displayResults = (filteredNames) => {
+    const results = document.getElementById("results");
+
+    if (filteredNames.length === 0) {
+        results.innerHTML = "<p>No meals found </p>";
+        return;
+    }
+
+    // join('') make array to a big string
+    const resultsList = filteredNames.map((name) => `<p>${name}</p>`).join("");
+
+    results.innerHTML = resultsList;
+};
+
+const filterMeals = (searchTerm) => {
+    if (searchTerm === "") {
+        document.getElementById("results").innerHTML = "";
+        return;
+    }
+
+    // keep only the names where .includes() return true
+    const filteredNames = allNames.filter((name) =>
+        name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    displayResults(filteredNames);
 };
 
 const init = async () => {
     try {
         const data = await fetchData(url);
+        allNames = data.recipes.map((recipe) => recipe.name);
 
-        const names = data.recipes.map((recipe) => recipe.name);
-
-        console.log(names);
+        // add event listener to the input
+        const searchInput = document.getElementById("searchInput");
+        searchInput.addEventListener("input", (e) => {
+            filterMeals(e.target.value);
+        });
     } catch (error) {
         console.error(error);
     }
